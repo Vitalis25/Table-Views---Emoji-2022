2fb667fa2667e7a0aa0dd29b71dd25be5a623b76
Vitaly <lub-olen@yandex.ru>
2022-07-07 23:09:48 +0300

    Initial Commit

---
+++ Table Views/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Table Views
+//
+//  Created by Vitally Ochnev on 07.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
